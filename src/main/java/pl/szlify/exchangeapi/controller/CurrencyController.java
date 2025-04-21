63d77066a1e2eb438db27396d677902a712a4441
ganisz66 <116847028+ganisz66@users.noreply.github.com>
2025-04-21 17:08:56 +0200

    External endpoints implementation

--- src/main/java/pl/szlify/exchangeapi/controller/CurrencyController.java
+++ src/main/java/pl/szlify/exchangeapi/controller/CurrencyController.java
@@ -1,11 +1,17 @@
 package pl.szlify.exchangeapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.szlify.exchangeapi.model.TestModel;
 import pl.szlify.exchangeapi.service.CurrencyService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +23,16 @@ public class CurrencyController {
     @GetMapping("/test")
     public TestModel test() {
         return currencyService.test();
+    }
+
+    @GetMapping("/convert")
+    public ResponseEntity<BigDecimal> convertCurrency(
+            @RequestParam String from,
+            @RequestParam String to,
+            @RequestParam BigDecimal amount,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        BigDecimal result = currencyService.convertCurrency(from, to, amount, date);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/pl/szlify/exchangeapi/service/CurrencyService.java
+++ src/main/java/pl/szlify/exchangeapi/service/CurrencyService.java
@@ -2,17 +2,65 @@ package pl.szlify.exchangeapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import pl.szlify.exchangeapi.model.TestModel;
 import pl.szlify.exchangeapi.properties.ExchangeApiProperties;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class CurrencyService {
 
     private final ExchangeApiProperties properties;
+    private final RestTemplate restTemplate;
 
     public TestModel test() {
         return new TestModel(properties.getBaseUrl(), properties.getApiKey());
     }
+
+    public BigDecimal convertCurrency(String from, String to, BigDecimal amount, LocalDate date) {
+        URI baseUri = URI.create(properties.getBaseUrl() + "/convert");
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .newInstance()
+                .uri(baseUri)
+                .queryParam("from", from)
+                .queryParam("to", to)
+                .queryParam("amount", amount);
+
+        if (date != null) {
+            uriBuilder.queryParam("date", date.toString());
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("apikey", properties.getApiKey());
+
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(
+                uriBuilder.build().toUri(),
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
+        Object resultObj = Objects.requireNonNull(response.getBody()).get("result");
+        return new BigDecimal(resultObj.toString());
+    }
+
+
+
 }
